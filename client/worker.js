663a9d2bd258bd83002e1b6017b0c505df3cb52e
Brian Chu <bc@brianchu.com>
2013-06-24 20:11:30 -0700

    Added npm deps, added indexeddb code

--- client/worker.js
+++ client/worker.js
@@ -1,6 +1,26 @@
+var db;
+var dbRequest = window.indexedDB.open("nodetron", 1);
+dbRequest.onerror = function(event) {
+  console.log(event); //some sort of alert informing the user that they failed to grant permissions
+  console.log(dbRequest.errorCode);
+};
+dbRequest.onsuccess = function(event) {
+  db = dbRequest.result;
+  db.onerror = function(event) {
+    console.log('database error: ' + event.target.errorCode);
+  };
+};
+dbRequest.onupgradeneeded = function(event) {
+  var db = event.target.result;
+
+  // Create an objectStore for this database
+  var objectStore = db.createObjectStore("user", { connectInfo: "info" });
+};
+
  var socket = io.connect('http://localhost');
   socket.on('users', function (data) {
     // socket.emit('my other event', { my: 'data' });
+
   });
 
 //handle requests from the main loop
