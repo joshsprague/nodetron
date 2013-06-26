978c20c466a8954e96175966a7d5704e2f2d7649
Brian Chu <bc@brianchu.com>
2013-06-26 16:24:06 -0700

    Work on tests

---
+++ test/client/unit/workerEvents.js
@@ -0,0 +1,19 @@
+describe('WorkerEvents', function() {
+
+  var worker;
+  beforeEach(function() {
+    worker = new Worker('workerInternalEvents.js');
+  });
+  afterEach(function() {
+    worker.terminate();
+  });
+
+  it('should handle a simple request for data', function(done) {
+    worker.postMessageWithCallback('getUsers', function(users) {
+      expect(users).to.be.an.intanceof([]);
+      done();
+    });
+  });
+
+});
+
