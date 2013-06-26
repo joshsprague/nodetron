f01940f8d0dd363ce1f575ddd8b1462e59e713d6
Brian Chu <bc@brianchu.com>
2013-06-26 03:21:18 -0700

    Removed test placeholder

--- test/client/unit/test.js
+++
@@ -1,9 +0,0 @@
-/*global describe, it */
-describe('Give it some context', function () {
-    describe('maybe a bit more context here', function () {
-        it('should run here few assertions', function () {
-          console.log('hoya');
-          expect(5).to.equal(5);
-        });
-      });
-  });
