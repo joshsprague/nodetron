43774de1e3973fcae71119bb287c462bd42c8bb0
Jake Seip <jake.seip@gmail.com>
2013-07-01 10:55:43 -0700

    refactored peerQuery to pass the full server response object back to the application developer

--- client/peerSetup.js
+++ client/peerSetup.js
@@ -54,7 +54,7 @@ nodetron.registerWithServer = function(options){
 nodetron.initiatePeerConnection = function(peerJSCon, peerID){
   var conn = peerJSCon.connect(peerID,{'metadata':options});
   conn.on('open', function() {
-    conn.send('Hello world!');
+    conn.send('Acknowledge');
     console.log("Connection Opened");
   });
   conn.on('error', function(err){
@@ -67,7 +67,6 @@ nodetron.initiatePeerConnection = function(peerJSCon, peerID){
 };
 
 nodetron.findPeer = function(socketCon, queryParam, callback){
-  console.log(queryParam);
   var queryID = Math.random(); //TODO - upgrade this to a proper uuid function like uuid.v4();
   nodetron.activeQueries =  nodetron.activeQueries || {};
   nodetron.activeQueries[queryID] = callback;
@@ -77,7 +76,7 @@ nodetron.findPeer = function(socketCon, queryParam, callback){
   var dispatchResponse = function(queryResponse){
     if(nodetron.activeQueries[queryResponse.queryID]){
       console.log("firing callback");
-      nodetron.activeQueries[queryResponse.queryID](queryResponse.userID); //fire the callback
+      nodetron.activeQueries[queryResponse.queryID](queryResponse); //fire the callback
       delete nodetron.activeQueries[queryID]; //remove it from the events hash
     }
   };
