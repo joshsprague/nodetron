0d604b8372a990ddc86948942712de09957719b7
Brian Chu <bc@brianchu.com>
2013-07-01 17:03:11 -0700

    Removed worker routing code

--- client/background/workerInternalResources.js
+++ client/background/workerInternalResources.js
@@ -1,58 +1,9 @@
-var allow = {
-  get:{
-    users:true
-  },
-  post:{
-  },
-  put:{
-  },
-  delete:{
-  }
-};
-var deny = {
-  get:{
-  },
-  post:{
-  },
-  put:{
-  },
-  delete:{
-  }
-};
-
-//if resource is undefined, all resources under that method will be toggled
-var setAllow = function(method,resource) {
-  if (!allow[method]) {
-    return postError('setAllow', 'Method does not exist!');
-  }
-  allow[method][resource] = true;
-};
-var setDeny = function(method,resource) {
-  if (!deny[method]) {
-    return postError('setDeny', 'Method does not exist!');
-  }
-  deny[method][resource] = true;
-};
-
 var router = function(msg) {
-  var method = msg.method;
+  // var method = msg.method;
   var resource = msg.resource; //model, type, resource
-  var command = msg.commmand;
-  if (command === 'setDeny') {
-    return setDeny(method,resource);
-  }
-  if (command === 'setAllow') {
-    return setAllow(method,resource);
-  }
-  if (!method || !resource) {
+  if (!resource) {
     return false;
   }
-  // if (deny[method] && deny[method][resource]) {
-  //   return false;
-  // }
-  // if (allow[method] && allow[method][resource]) {
-  //   return true;
-  // }
   return true;
 };
 
