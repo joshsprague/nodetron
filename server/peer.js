7098011b7d3a7d6df3ea66629fe06a865ed01043
Brandon <bsalazar91@gmail.com>
2013-07-08 19:04:15 -0700

    More tests and refactor of old ones

--- server/peer.js
+++ server/peer.js
@@ -2,7 +2,7 @@ var PeerServer = require("./server.js").PeerServer;
 var options = {
   port: process.env.PORT || 5000,
   debug: true, //Enable server logs
-  mongo: process.env.MONGOLAB_URI || process.env.MONGOHQ_URL || "mongodb://localhost/test"
+  mongo: "mongodb://localhost/nodetron"
 };
 var server = new PeerServer(options);
 console.log('Running on port: ', options.port);
