0527e0d304bc7824a0e3adb07666f689d28c68ad
Brian Chu <bc@brianchu.com>
2013-07-09 14:26:49 -0700

    Removed IIFE from workerExternal

--- client/background/workerExternal.js
+++ client/background/workerExternal.js
@@ -1,25 +1,22 @@
-(function(window) {
-  var nodetron = window.nodetron || (window.nodetron = {});
-  nodetron.worker = new Worker('scripts/nodetron/background/workerInternal.js');
+nodetron.worker = new Worker('scripts/nodetron/background/workerInternal.js');
 
-  // Normally workers pass objects by copy, not reference.
-  // worker can be passed other types like File, Blob, ArrayBuffer/
-  nodetron.init = function(params) {
-    var msg = {
-      init:true,
-      uuid:uuid,
-      registered:registered,
-      dbTitle:params.dbTitle,
-      dbVersion:params.dbVersion,
-      serverUrl:params.serverUrl
-    };
-    nodetron.worker.postMessage(msg);
-    nodetron.worker.addEventListener('message', function(event) {
-      console.log('Message to main!');
-      console.log(event);
-    });
+// Normally workers pass objects by copy, not reference.
+// worker can be passed other types like File, Blob, ArrayBuffer/
+nodetron.init = function(params) {
+  var msg = {
+    init:true,
+    uuid:uuid,
+    registered:registered,
+    dbTitle:params.dbTitle,
+    dbVersion:params.dbVersion,
+    serverUrl:params.serverUrl
   };
+  nodetron.worker.postMessage(msg);
+  nodetron.worker.addEventListener('message', function(event) {
+    console.log('Message to main!');
+    console.log(event);
+  });
+};
 
-  //to pass objects by reference, do worker.postMessage(data, <array of ArrayBuffers>);
-  //https://zapier.com/engineering/intro-to-web-workers/
-})(this);
+//to pass objects by reference, do worker.postMessage(data, <array of ArrayBuffers>);
+//https://zapier.com/engineering/intro-to-web-workers/
