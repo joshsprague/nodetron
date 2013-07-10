e4c803df1ab44b849a1a6a9159e380dcacda6636
Brian Chu <bc@brianchu.com>
2013-07-10 15:04:26 -0700

    Moved nodetron peer.js server to demo

--- server/peer.js
+++
@@ -1,7 +0,0 @@
-var Nodetron = require("./server").NodetronServer;
-var options = {
-  port: 5000,
-  debug: true //Enable server logs
-};
-var server = new Nodetron(options);
-console.log('Running on port: ', options.port);
